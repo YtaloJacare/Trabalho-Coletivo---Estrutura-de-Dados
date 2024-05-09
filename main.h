ea726132fb6011d553336ae02135e78960f50d9f
KaioCesaar <joao.rmartins@p.ucb.br>
2024-05-09 20:06:27 -0300

    Update main.h
    Esvaziando os parâmetros

--- main.h
+++ main.h
@@ -4,14 +4,14 @@
 typedef struct Quarto;
 
 void limparBuffer();
-void limparString(char *str);
-void iniciarLista(Quarto quarto[], int *posicaoQuarto);
-void reescreverLista(Quarto quarto[], int quantidadeQuartos);
-void inserirHospede(Quarto quarto[], int *posicaoQuarto);
-void buscarHospede(Quarto quarto[], int quantidadeQuartos);
-void editarHospede(Quarto quarto[], int quantidadeQuartos);
-void liberarQuarto(Quarto quarto[], int quantidadeQuartos);
-void mostrarQuartosVazios(Quarto quarto[], int quantidadeQuartos);
-void listarHospedes(Quarto quarto[], int quantidadeQuartos);
+void limparString();
+void iniciarLista();
+void reescreverLista();
+void inserirHospede();
+void buscarHospede();
+void editarHospede();
+void liberarQuarto();
+void mostrarQuartosVazios();
+void listarHospedes();
 
 #endif
